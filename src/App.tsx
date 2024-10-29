154c78c4c87ea7050c5a4b583bc49e144207b5e5
Ahmed Abas Al-Ali <bfhetyrhyr12@gmail.com>
2024-10-29 09:32:23 +0300

    add: deploy workflow

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const routesConfig = [
 
   }
 ]
-const router = createBrowserRouter(routesConfig);
+const router = createBrowserRouter(routesConfig, { basename: '/IBMU' });
 
 
 const App = () => {
